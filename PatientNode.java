7cdd2de2336ca55bab519e09e93fe606d247370c
Mohammed Afrath <afrathmmm1212@gmail.com>
2026-09-06 23:25:17 +0530

    Added Patient BST Node

---
+++ PatientNode.java
@@ -0,0 +1,14 @@
+public class PatientNode {
+
+    Patient patient;
+
+    PatientNode left;
+    PatientNode right;
+
+    public PatientNode(Patient patient) {
+
+        this.patient = patient;
+        left = null;
+        right = null;
+    }
+}
